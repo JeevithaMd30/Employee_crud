42c8147a7f29760bff5a847a6a613cebe03d4b94
Jeevitha MD <jeevitha.md@sysforetech.local>
2024-01-22 11:54:44 +0530

    Revert "Added comment in Employeeinerface"
    This reverts commit 9913624e990cfafd5bf6bf853d358a18b4972eef.

--- IServices/IEmployeeServices.cs
+++ IServices/IEmployeeServices.cs
@@ -4,20 +4,13 @@ namespace CRUD_with_SP.IServices
 {
     public interface IEmployeeServices
     {
-        // by creating interface we can acccess the methods anywhere in the project
          List<EmpModel> GetAllEmployees();
-
-
         // public Task GetEmpBy(int id);
-         EmpModel GetEmpById(int id);
-
+          EmpModel GetEmpById(int id);
          void AddEmployeeData(EmpModel employee);
+       EmpModel UpdateEmployee(EmpModel emp1);
 
-         EmpModel UpdateEmployee(EmpModel emp1);
-
-       //EmpModel EditPage(int id);
-
-        void DeleteEmployee(int id);  
-        
+        EmpModel EditPage(int id);
+        void DeleteEmployee(int id);
     }
 }
